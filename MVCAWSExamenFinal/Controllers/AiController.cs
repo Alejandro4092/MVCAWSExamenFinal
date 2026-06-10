6c3fb100a7180707e8a25cc8db563cb355c84ac5
Alejandro4092 <acobomarcos@gmail.com>
2026-06-10 12:56:37 +0200

    bbb

---
+++ MVCAWSExamenFinal/Controllers/AiController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCAWSExamenFinal.Services;
+
+namespace MVCAWSExamenFinal.Controllers
+{
+    public class AIController : Controller
+    {
+        private readonly AiService _ai;
+
+        public AIController(AiService ai)
+        {
+            _ai = ai;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string question)
+        {
+            var answer = await _ai.AskAsync(question);
+
+            ViewData["answer"] = answer;
+
+            return View();
+        }
+    }
+}
